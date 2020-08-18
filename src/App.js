75ad09119b928c85d908654e8400a54a335df2de
Aarón Parres López <aaronplmultimedia@gmail.com>
2020-08-18 19:41:59 +0200

    First try rendering data useState + useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
+
+import data from './backend/devaway-racing-services-export.json';
 
 const App = () => {
+  const { driversData } = data;
+
+  const [drivers, setDrivers] = useState(driversData);
+
+  useEffect(() => {
+    console.log(drivers);
+  }, []);
+
   return (
-    <div>
-      Hello there
-    </div>
+    <Fragment>
+      {drivers && drivers.map((driver, i) => (<p key={i}>{driver.name}</p>))}
+      GLOBAL RANKING
+      Race 1 .... n
+      Driver 1 ... n
+    </Fragment>
+
   );
 }
 
