e4d824316ffbc4a9629969e89c28ee3389793bff
Aarón Parres López <aaronplmultimedia@gmail.com>
2020-08-20 05:01:10 +0200

    Driver component now gets driver and races info
    Filtered using the id from the urlParam

--- src/components/Driver/Driver.js
+++ src/components/Driver/Driver.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-const Driver = ({ driversList, racesResults }) => {
+const Driver = ({ racesResults, driversRanking }) => {
     const { id } = useParams();
-    console.log('driversList', driversList)
-    console.log('racesResults', racesResults)
+
+    const [selectedDriver, setSelectedDriver] = useState({});
+    const [driverRaces, setDriverRaces] = useState([]);
+
+    useEffect(() => {
+        // Get driver info by id
+        const driverInfo = driversRanking.filter(position => position._id === id);
+        setSelectedDriver(...driverInfo);
+
+        // Get driver races info by id
+        const driverRaces = [];
+        racesResults.forEach(race => {
+            race.forEach(position => {
+                if (position._id === id) driverRaces.push(position);
+            })
+        });
+        setDriverRaces(driverRaces);
+
+    }, [racesResults, driversRanking, id]);
+
     return (
         <div className="container">
             <div className="card">
-                {id}
+                {selectedDriver.name}
+                Global position: {selectedDriver.globalPosition}
+                {driverRaces.map((race, i) => (
+                    <div key={i}>
+                        <p>Race {i + 1} - pos: {race.positionInRace}</p>
+                    </div>
+                ))}
             </div>
         </div>
     )
