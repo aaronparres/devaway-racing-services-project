5a569047f78525f4272e309b3a473212c69391a3
Aarón Parres López <aaronplmultimedia@gmail.com>
2020-08-22 03:47:19 +0200

    Created dropdown for the Races on GlobalRanking

--- src/components/GlobalRankingList/GlobalRankingList.js
+++ src/components/GlobalRankingList/GlobalRankingList.js
@@ -3,11 +3,24 @@ import React from 'react';
 import './GlobalRankingList.scss';
 
 import RankingCard from './RankingCard/RankingCard';
+import { Link } from 'react-router-dom';
 
-const GlobalRankingList = ({ driversRanking }) => {
+const GlobalRankingList = ({ driversRanking, races }) => {
     return (
         <div className="container-fluid">
             <h1 className="center-text">GLOBAL RANKING</h1>
+            {races &&
+                <div className="container">
+                    <div className="dropdown">
+                        <button className="btn btn-danger dropdown-toggle">See Races info</button>
+                        <div className="dropdown-content">
+                            {races && races.map((race, i) => (
+                                <Link key={i} to={`/race/${i + 1}`}>Race {i + 1}</Link>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            }
             {driversRanking && driversRanking.map((driver, i) => (
                 <RankingCard key={i} driverInfo={driver} />
             ))}
